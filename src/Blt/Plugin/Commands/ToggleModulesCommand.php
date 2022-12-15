0ffc5c74de13c4b08603eaa41498191e7eb73d0c
Ian Monroe <ian@ianmonroe.com>
2022-12-15 11:22:56 -0700

    Continue on error if one is thrown

--- src/Blt/Plugin/Commands/ToggleModulesCommand.php
+++ src/Blt/Plugin/Commands/ToggleModulesCommand.php
@@ -7,6 +7,7 @@ use Acquia\Blt\Robo\BltTasks;
 use Acquia\Blt\Robo\Common\UserConfig;
 use Acquia\Blt\Robo\Common\YamlMunge;
 use Acquia\Blt\Robo\Exceptions\BltException;
+use Robo\ResultData;
 use Zumba\Amplitude\Amplitude;
 
 /**
@@ -41,8 +42,8 @@ class ToggleModulesCommand extends BltTasks {
       // Uninstall modules.
       $disable_key = "modules.$environment.uninstall";
       $this->doToggleModules('pm-uninstall', $disable_key);
-    }
-    else {
+
+    } else {
       $this->say("Environment is unset. Skipping drupal:toggle:modules...");
     }
   }
@@ -81,7 +82,8 @@ class ToggleModulesCommand extends BltTasks {
     }
 
     if ($exit_code) {
-      throw new BltException("Could not toggle modules listed in $config_key.");
+      $this->say("There was a problem toggling modules listed in $config_key. You may want to check the outcome manually.");
+      return new ResultData(0);
     }
   }
 
